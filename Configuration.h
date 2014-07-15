a631818036cf2aabc813f14602289ddb1fc594b3
Scott Gibson <scott.gibson@ericsson.com>
2014-07-15 16:00:50 -0400

    Update config

--- Configuration.h
+++ Configuration.h
@@ -350,7 +350,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
 //#define MANUAL_Z_HOME_POS 383.8 // For delta: Distance between nozzle and print surface after homing.
-#define MANUAL_Z_HOME_POS 391.6 
+#define MANUAL_Z_HOME_POS 400 
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
